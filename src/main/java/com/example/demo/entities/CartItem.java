0d78d1cdf35ff8a784e609c14a0b2d84b75f073b
jlitt83 <jlitt83@wgu.edu>
2023-12-20 18:59:49 -0800

    Part D - Created Entity Package and Classes

---
+++ src/main/java/com/example/demo/entities/CartItem.java
@@ -0,0 +1,43 @@
+package com.example.demo.entities;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.util.Date;
+import java.util.Set;
+
+
+@Entity
+@Table(name="CartItem")
+@Getter
+@Setter
+
+public class CartItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name= "cart_item_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name= "vacation_id", nullable = false)
+    private Vacation vacation;
+
+    @ManyToMany(mappedBy = "cartitems")
+    private Set<Excursion> excursions;
+
+    @ManyToOne
+    @JoinColumn(name= "cart_id", nullable = false)
+    private Cart cart;
+
+    @Column(name= "create_date")
+    @CreationTimestamp
+    private Date create_date;
+
+    @Column(name= "last_update")
+    @UpdateTimestamp
+    private Date last_update;
+
+}
